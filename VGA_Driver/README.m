e47f0c4444f8d458b18dc15e135dea4b0261b270
ORHAN YILMAZ <ornyilmaz@gmail.com>
2023-01-04 09:47:23 +0300

    Update README.m

--- VGA_Driver/README.m
+++ VGA_Driver/README.m
@@ -2,4 +2,4 @@ VGA Modülü
 
 Simülasyon Ekranı
 ---
-![Simülasyon Çıktısı](https://www.mafgom.com/wp-content/uploads/2011/08/vga_modelsim1.png)
+![alt text](https://www.mafgom.com/wp-content/uploads/2011/08/vga_modelsim1.png)
